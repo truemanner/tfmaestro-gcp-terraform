3e97a59e4858a49502abe2d34bee88632e3cba0f
Krzysztof Manijak <mannerek@gmail.com>
2026-01-04 15:57:18 +0100

    Dodaj plik .gitignore oraz przykładowe wartości zmiennych do terraform.tfvars.example; zaktualizuj opisy zmiennych w pliku variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,16 @@
-variable "project_id" {}
-variable "region" {
-  default = "europe-central2"
+variable "project_id" {
+  description = "ID projektu GCP, w którym tworzone są zasoby"
+  type        = string
 }
+
+variable "region" {
+  description = "Domyślny region GCP"
+  type        = string
+  default     = "europe-central2"
+}
+
 variable "zone" {
-  default = "europe-central2-a"
+  description = "Domyślna strefa GCP dla instancji Compute Engine"
+  type        = string
+  default     = "europe-central2-a"
 }
